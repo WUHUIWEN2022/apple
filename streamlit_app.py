4d2ebc2bdda15a4af4dc1f0733721e4aa28eac30
WUHUIWEN2022 <118030512+WUHUIWEN2022@users.noreply.github.com>
2022-11-11 20:49:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,8 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥣 🥗 🐔 🥑🍞') 
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
