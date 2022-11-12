b6bd9fe8c63391e8aa86e7837be47e1ca14d1878
WUHUIWEN2022 <118030512+WUHUIWEN2022@users.noreply.github.com>
2022-11-11 20:46:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ streamlit.title("hahahah")
 streamlit.header("no no ")
 
 streamlit.text( "yayaya")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 🥗 🐔 🥑🍞') 
