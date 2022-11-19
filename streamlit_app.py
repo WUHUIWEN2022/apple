7cb43888eaa75b75b9c8fd0c01552fb3a85d4110
WUHUIWEN2022 <118030512+WUHUIWEN2022@users.noreply.github.com>
2022-11-19 11:19:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import urlerror
+from urllib.error import URLError
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
